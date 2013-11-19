936a3e892d4ca98dcb019324dc141026a3252c19
ktc-jenkins <jenkins.ktcloudware@gmail.com>
2013-11-19 15:09:11 +0900

    auto_version to 0.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "taeilchoi1@gmail.com"
 license           "Apache 2.0"
 description       "Add configs for sending logs to central logstash server"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.1.3'
 
 recipe            "default", "Add configs for sending logs to central logstash server"
 
